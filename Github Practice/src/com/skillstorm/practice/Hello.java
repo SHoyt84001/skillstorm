c8929e692031f6f2c383d39186f0b61fc9cb582f
Steven Hoyt <steven.hoyt96@gmail.com>
2023-11-20 16:26:22 -0500

    adding assignment

--- Github Practice/src/com/skillstorm/practice/Hello.java
+++ Github Practice/src/com/skillstorm/practice/Hello.java
@@ -2,8 +2,11 @@ package com.skillstorm.practice;
 
 public class Hello {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String [] args) {
+	}
+	
+	public static void helloWorld(String[] args) {
 		System.out.println("Hello world!");
 	}
 }
+
